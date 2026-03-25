985ee677926ce96eabf917cf453eac77718161c2
pdfboltapp <pdfbolt.app@gmail.com>
2026-03-25 23:37:24 +0100

    Add files via upload

---
+++ pdfbolt-deploy/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://pdfbolt.app",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+  ];
+}
